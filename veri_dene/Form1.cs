cba3390a43439297ffff64bbf62928924dfc0cea
ipekakdg <ipekakdg48@gmail.com>
2022-08-25 20:13:47 +0300

    Proje dosyası ekle.

---
+++ veri_dene/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Net;
+namespace veri_dene
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string adres = "https://razerid.razer.com/?client_id=63c74d17e027dc11f642146bfeeaee09c3ce23d8&redirect=https%3A%2F%2Fgold.razer.com%2F";
+            WebRequest istek = HttpWebRequest.Create(adres); //adres alanındaki web isteği için oluştur.
+            WebResponse cevap;
+            cevap = istek.GetResponse();
+            StreamReader donenbilgiler = new StreamReader(cevap.GetResponseStream());
+            string gelendegerler = donenbilgiler.ReadToEnd();//gelen bilgiler ile birlikte donen bilgileri sonlandır.
+            int baslıkbaslangic = gelendegerler.IndexOf("<title>") + 7; // başlık kaç karakterden oluşacak bilgisi 
+            int baslıkbitisi = gelendegerler.Substring(baslıkbaslangic).IndexOf("</title>");
+            string baslik = gelendegerler.Substring(baslıkbaslangic, baslıkbitisi);
+            MessageBox.Show(baslik);
+
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Form2 form2 = new Form2();
+            form2.Show();  // form2 göster diyoruz
+            this.Hide();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ veri_dene/Form2.Designer.cs
@@ -0,0 +1,72 @@
+﻿
+namespace veri_dene
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.data = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // data
+            // 
+            this.data.Location = new System.Drawing.Point(460, 17);
+            this.data.Name = "data";
+            this.data.Size = new System.Drawing.Size(104, 36);
+            this.data.TabIndex = 2;
+            this.data.Text = "verileri getir";
+            this.data.UseVisualStyleBackColor = true;
+            this.data.Click += new System.EventHandler(this.data_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 15;
+            this.listBox1.Location = new System.Drawing.Point(33, 103);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(530, 229);
+            this.listBox1.TabIndex = 3;
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(597, 372);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.data);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button data;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
